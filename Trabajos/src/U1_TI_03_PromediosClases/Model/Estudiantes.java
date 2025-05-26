d72bbc0dd95266b4a776919df32a9a4072743bc3
Daniel Emiliano Hernández Arroyo <20243ds075@utez.edu.mx>
2025-05-25 19:30:27 -0600

    Tarea del día 25/5/25, es un calculo de promedios mediante una clase estudiantes, usando metodos publicos y atributos privados

---
+++ Trabajos/src/U1_TI_03_PromediosClases/Model/Estudiantes.java
@@ -0,0 +1,68 @@
+package U1_TI_03_PromediosClases.Model;
+
+public class Estudiantes {
+
+    private String studentName;
+    private String studentId;
+    private double[] studentGrades = new double[5];
+
+    public String getStudentName() {
+
+        return studentName;
+
+    }
+
+    public String getStudentId(){
+
+        return studentId;
+
+    }
+
+    public double getGrade(int index){
+
+        return studentGrades[index];
+
+    }
+
+    public double getAverage() {
+
+        double sum = 0;
+
+        for(int i = 0; i < studentGrades.length; i++) {
+
+            sum += studentGrades[i];
+
+        }
+
+        double average = sum / 5;
+
+
+        return average;
+
+    }
+
+    public void setStudentName(String studentName){
+
+        this.studentName = studentName;
+
+    }
+
+    public void setStudentId(String studentId) {
+
+        this.studentId = studentId;
+
+    }
+
+    public void setStudentGrade(int index, double grade){
+
+        this.studentGrades[index] = grade;
+
+    }
+
+    public boolean isStudentPassed(){
+
+        return getAverage() >= 70;
+
+    }
+
+}
