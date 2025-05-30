98dd458ab38933e2d246339f990f9b3d8f51a22e
Daniel Emiliano Hernández Arroyo <20243ds075@utez.edu.mx>
2025-05-29 20:24:25 -0600

    Implementa clase UsuarioSeguro con validaciones de contraseña y pruebas de autenticación en main

--- Trabajos/src/U1_TI_06_ClaseUsuario_Con_Contrasenia/Login.java
+++ Trabajos/src/U1_TI_06_ClaseUsuario_Con_Contrasenia/Login.java
@@ -1,15 +1,23 @@
 package U1_TI_06_ClaseUsuario_Con_Contrasenia;
 
-public class Main {
+import javax.swing.JOptionPane;
+import U1_TI_06_ClaseUsuario_Con_Contrasenia.Model.UsuarioSeguro;
+
+public class Login {
 
     public static void main(String[] args) {
 
+        UsuarioSeguro usuario = new UsuarioSeguro();
 
+        String username = JOptionPane.showInputDialog("Introduce tu nombre de usuario:");
+        usuario.setNombreUsuario(username);
 
+        String password = JOptionPane.showInputDialog("Introduce tu contraseña:");
+        usuario.setPassword(password);
 
+        usuario.autenticar(password);
 
-
-
+        usuario.autenticacionMensaje(password);
 
     }
 
