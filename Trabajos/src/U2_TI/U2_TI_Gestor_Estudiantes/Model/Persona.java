16bbade56172375defafffd2a08325f48b8e8f98
Daniel Emiliano Hernández Arroyo <20243ds075@utez.edu.mx>
2025-06-15 21:23:24 -0600

    Se cambio la ubicacion de la carpeta dentro del repo a U2, pa que no se pierda dentro de todas las carpetas.

---
+++ Trabajos/src/U2_TI/U2_TI_Gestor_Estudiantes/Model/Persona.java
@@ -0,0 +1,39 @@
+package U2_TI.U2_TI_Gestor_Estudiantes.Model;
+
+public abstract class Persona {
+
+    private String nombre;
+    private String email;
+
+    public Persona(String nombre, String email) {
+
+        this.nombre = nombre;
+        this.email = email;
+
+    }
+
+    public String getNombre() {
+
+        return nombre;
+
+    }
+
+    public void setNombre(String nombre) {
+
+        this.nombre = nombre;
+
+    }
+
+    public String getEmail() {
+
+        return email;
+
+    }
+
+    public void setEmail(String email) {
+
+        this.email = email;
+
+    }
+
+}
---
+++ Trabajos/src/U2_TI/U2_TI_Gestor_Estudiantes/Model/Profesor.java
@@ -0,0 +1,11 @@
+package U2_TI.U2_TI_Gestor_Estudiantes.Model;
+
+public class Profesor extends Persona{
+
+    public Profesor(String nombre, String email) {
+
+        super(nombre, email);
+
+    }
+
+}
